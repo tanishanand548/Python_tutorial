3bf1ed89308d4dfc7d2d9cafa980be934a5c9962
Tanish Anand <tanishanand548@gmail.com>
2024-06-02 21:03:38 +0530

    python comments

---
+++ #Day-02{comments}.py
@@ -0,0 +1,10 @@
+# Python Comments
+# In simple word, comments is the bypassing the syntax or text when the code is executed
+
+# Uses of comments
+
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+# print("hello world!"), since '#' is denoted as comments in python
