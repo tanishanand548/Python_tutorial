2b7dfcea3f42820f83984940fb4746a1c646a1de
Tanish Anand <tanishanand548@gmail.com>
2024-06-02 21:03:04 +0530

    syntax and indentation in python

---
+++ #Day-01{syntax & indentation}.py
@@ -0,0 +1,11 @@
+# Syntax & first program {"Hello World"}
+print("Hello World!") #o/p:- Hello World!
+
+# Python Indentation
+# Indentation refers to the spaces at the beginning of a code line.
+# eg:-
+     print("Python tutorial") #o/p:- its give an indentation error, because of space at the starting or beginning
+     
+# Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+
+# Python uses indentation to indicate a block of code.
