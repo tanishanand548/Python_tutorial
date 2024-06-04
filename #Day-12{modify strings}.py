dfb26c3128514fa59f2ad8eec0b486014ec0bc4b
Tanish Anand <tanishanand548@gmail.com>
2024-06-05 01:59:36 +0530

    modify strings

---
+++ #Day-12{modify strings}.py
@@ -0,0 +1,17 @@
+# Python - Modify Strings
+
+'''Upper Case'''
+p = " python tutorial"
+print(p.upper())
+
+'''Lower Case'''
+print(p.lower())
+
+'''Remove Whitespace'''
+print(p.strip()) # "strip()" method removes any whitespace from the beginning or the end
+
+'''Replace String'''
+print(p.replace('py', 'jy'))
+
+'''Split String'''
+print(p.split()) # break and formed a list items
