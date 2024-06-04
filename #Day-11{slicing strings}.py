e30d80d0b5c0dc478e5647fff49a943c7c24aac9
Tanish Anand <tanishanand548@gmail.com>
2024-06-05 01:59:16 +0530

    slicing strings

---
+++ #Day-11{slicing strings}.py
@@ -0,0 +1,7 @@
+# Python - Slicing Strings
+# You can return a range of characters by using the slice syntax.
+
+a = "python tutorial"
+print(a[1:5]) #last character is a blank space or more way is "5-1" = "4", so return only "4" character
+
+print(a[1:]) #return all the character at index "1" to till the end
