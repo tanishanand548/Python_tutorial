9bf65bca99c1ca31ad24d676a968397f810a57fd
Tanish Anand <tanishanand548@gmail.com>
2024-06-05 02:00:40 +0530

    escape characters

---
+++ #Day-15{escape characters}.py
@@ -0,0 +1,24 @@
+# Python - Escape Characters
+'''insert characters that are illegal in a string, use an escape character'''
+'''escape character is a backslash "\" followed by the character you want to insert'''
+
+# txt = "We are the so-called "Vikings" from the north." 
+# #its give an error
+
+# fix the error by using escape character
+
+txt = "We are the so-called \"Vikings\" from the north."
+
+'''Some other escape character used in python are as follow'''
+
+'''
+\' - Single Quote
+\\ - Backslash
+\n - New Line
+\r - Carriage Return
+\t - Tab
+\b - Backspace
+\f - Form Feed
+\ooo - Octal value {text are in octal, convert into readable format}
+\xhh - Hex value {text are in hex, convert into readable format}
+'''
