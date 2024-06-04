4c7a6f0a21015259c934604697565f171a182fd4
Tanish Anand <tanishanand548@gmail.com>
2024-06-05 02:01:04 +0530

    strings methods

---
+++ #Day-16{strings methods}.py
@@ -0,0 +1,5 @@
+# Python - String Methods
+# All string methods return new values. They do not change the original string
+
+'''Here the link to learn string methods'''
+https://www.w3schools.com/python/python_strings_methods.asp
