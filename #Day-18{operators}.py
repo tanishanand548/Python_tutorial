d1ca114d154c8356d28c42497efe1403570c0659
Tanish Anand <tanishanand548@gmail.com>
2024-06-14 23:55:03 +0530

    operators

---
+++ #Day-18{operators}.py
@@ -0,0 +1,7 @@
+# Python Operators
+'Operators are used to perform operations on variables and values'
+
+https://www.w3schools.com/python/python_operators.asp
+
+# eg:-
+# print( 5 + 5) "+" is the operator which perform addition operation
