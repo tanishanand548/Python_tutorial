b68c0ade50c35a528e50f213ac32704b72e1f969
Tanish Anand <tanishanand548@gmail.com>
2024-06-19 15:03:40 +0530

    copy lists

---
+++ #Day-27{copy lists}.py
@@ -0,0 +1,11 @@
+# Copy Lists
+
+'''Make a copy of a list with the copy() methods'''
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# Make a copy of a list with the list() method
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
