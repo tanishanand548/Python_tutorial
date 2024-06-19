d815c04882931de20393c718c146008a8bcf0219
Tanish Anand <tanishanand548@gmail.com>
2024-06-19 15:04:41 +0530

    join lists

---
+++ #Day-28{join lists}.py
@@ -0,0 +1,26 @@
+# Join Lists
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+# Append list2 into list1
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+# Use the extend() method to add list2 at the end of list1
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+# Reference of Python - List Methods
+# https://www.w3schools.com/python/python_lists_methods.asp
